e72be3f6f5a0541875cf50290b41b74582a0047b
José Alejandro Trejo <jose.atrejoc@hotmail.com>
2025-04-04 13:16:04 -0600

    update
    update

---
+++ stackbit.config.ts
@@ -0,0 +1,7 @@
+import type { StackbitConfig } from '@stackbit/types';
+
+export default {
+  stackbitVersion: '~0.5.0',
+  devCommand: 'npm run dev',
+  nodeVersion: '18',
+} satisfies StackbitConfig;
